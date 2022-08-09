ba42fbda102e3cd2a84c059f4ec937362579844e
DevanshSK <devanshs843@gmail.com>
2022-08-09 17:42:22 +0530

    Added Animations
    Added animations using GSAP library

---
+++ app.js
@@ -0,0 +1,12 @@
+const card = document.querySelector('.card');
+  window.addEventListener("load", () => {
+
+    // Card animation
+    gsap.fromTo(card, {scale: 0}, {scale: 1, duration: 1, ease: "elastic.out(1.5, 1)"});
+
+    // Footer animation
+    gsap.set('.letter', {display: "inline-block"});
+
+    gsap.fromTo('.letter',{y:"100%", opacity: 0} ,{y:0, duration: 0.75, opacity: 1, delay: 2, stagger: 0.05, ease: "back.out(3)"});
+
+  });
